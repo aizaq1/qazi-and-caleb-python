2d47bbc48d10975f541b3e4b881cf811df053bf9
aizaq1 <qaziashikin@gmail.com>
2022-06-27 16:16:02 -0400

    notes from 06/27

---
+++ test.py
@@ -0,0 +1,59 @@
+# Data types are ways for us to store information
+
+# Integers - whole numbers (negative or positive)
+# 2
+# 3
+# 4
+# ...
+# -2
+# -3
+# -4
+
+# Strings - store phrases or words that we want to have somewhere ; in quotations
+# greeting = "Hello world"  # Variable
+# print(greeting)
+
+# print("\"Hello world\"")
+# print('"Hello world"')
+
+# Floats - decimal numbers
+# 3.5
+# 4.0
+# 3.3
+# 2.0
+# 1.1
+# rating = 4.9
+# print(3.5)
+# print(2.0)
+# print(rating)
+
+# Booleans - True / False
+# True - capitilize
+# False - capitilize
+
+# Avoid the following
+# true = "Hello"
+# print(true)
+# false = "Hi"
+# print(false)
+# Avoid the above
+
+# 1 + 2  # addition
+# 3 - 1  # subtraction
+# 3 * 2  # multiplication
+# 6 / 3  # division
+
+# print(6/3*2+2-1*5)
+# 6/3*2+2-1*5
+# 2*2+2-1*5
+# 4+2-1*5
+# 4+2-5
+# 6-5
+# 1
+# print(1+2)
+# print(3-1)
+# print(3*2)
+
+# Input() allows users to enter infor
+# mation
+input("Enter your age: ")
