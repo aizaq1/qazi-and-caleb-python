e76f5356901825bda61b80fe809fa34e950d917e
aizaq1 <qaziashikin@gmail.com>
2022-07-05 16:01:43 -0400

    7/5 Notes

--- datatypes.py
+++ datatypes.py
@@ -56,4 +56,45 @@
 
 # Input() allows users to enter infor
 # mation
-input("Enter your age: ")
+# input("Enter your age: ")
+
+## REVIEW ##
+# True - Boolean
+# False - Boolean
+# "Hi my name is John." - String
+# 4.9 - Float
+# 17 - Integer
+# -20 - Integer
+# print("9"*3)  # 999
+# print("3"*3)  # 333
+# print("5" * 5)  # 55555
+
+# Type casting
+# age = int(input("Enter your age: "))  # Input() is always going to make it a String
+# print(age + 5)
+
+# rating = float(input("Enter your rating: "))
+# print(rating)
+
+# answer = input("Do you like ice cream? ")
+# print(answer)
+
+# age = input("What is your age? ")
+# # print("You are 9 years old")  # We want this
+# print("You are " + age + " years old.")  # String concatenation
+
+# answer = input("Do you like ice cream? ")
+# Case 1: answer = "Yes"
+# "Your answer was: Yes"
+# Case 2: answer = "No"
+# "Your answer was: No"
+# print("Your answer was: " + answer)
+
+# rating = float(input("Enter your rating: "))
+# "You rated the restaurant _____ stars."
+# # print("You rated the restaurant " + rating + " stars.")
+# print(f"You rated the restaurant {rating} stars.")  # Formatted string
+
+# answer = input("Do you like ice cream? ")
+# print("Your answer was: " + answer)
+# print(f"Your answer is : {answer}")
