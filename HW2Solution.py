bdf799309d1bfec19feeb674cc7278fc132c0444
aizaq1 <qaziashikin@gmail.com>
2022-07-12 16:05:44 -0400

    Solutions to HW 2

---
+++ HW2Solution.py
@@ -0,0 +1,27 @@
+# Question 1
+# 1. Can you make a program that asks the person their name? The program will then say their name back in a nice way. For example:
+
+# Enter your name: Qazi
+# Hi, Qazi! How are you?
+
+# String Concatenation
+name = input("Enter your name: ")
+print("Hi, " + name + "! How are you?")
+
+# Formatted String
+name = input("Enter your name: ")
+print(f"Hi, {name}! How are you?")
+
+# Question 2
+# 2. Can you make a program that asks the person their favorite hobby? The program will then say their hobby back in a nice way. For example:
+
+# Enter your favorite hobby: biking
+# Nice! I like biking too!
+
+# String Concatenation
+hobby = input("Enter your favorite hobby: ")
+print("Nice! I like " + hobby + " too!")
+
+# Formatted String
+hobby = input("Enter your favorite hobby: ")
+print(f"Nice! I like {hobby} too!")
